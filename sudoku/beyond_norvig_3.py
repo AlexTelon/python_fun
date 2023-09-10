7bf908b665b63b8f1928e1df74940ea4f3acc188
Alex Telon <alex.telon@gmail.com>
2023-09-10 22:45:12 +0200

    Added another variation on beyond_norvig_3

--- sudoku/beyond_norvig_3.py
+++ sudoku/beyond_norvig_3.py
@@ -9,6 +9,13 @@ units = list(chain(
     [*boxes(['ABC','DEF','GHI'], ['123','456','789'])]  # boxes
 ))
 
+# Variation
+# units = [
+#     *[combine('ABCDEFGHI', d) for d in '123456789'],    # columns
+#     *[combine(a, '123456789') for a in 'ABCDEFGHI'],    # rows
+#     *boxes(['ABC','DEF','GHI'], ['123','456','789'])    # boxes
+# ]
+
 # Get all unique cells seen in any of the units.
 squares = sorted(set(chain.from_iterable(units)))
 print(squares)
